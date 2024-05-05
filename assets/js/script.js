5f159272caf690b931ca5aa22af08578661c7597
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2024-05-05 16:30:03 +0700

    Complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,54 @@ document.querySelector(".header .button--dropdown").addEventListener("click", ()
 });
 
 document.querySelector(".header .button--close").addEventListener("click", () => {
-  document.querySelector(".header__menu").classList.remove("header__menu--show"); 
+  document.querySelector(".header__menu").classList.remove("header__menu--show");
 })
 // End Header
 
 // About
-let deg = 1;
+const shapeOne = document.querySelector(".about .shape--one");
+let aboutDeg = 1;
 setInterval(() => {
-  document.querySelector(".about .shape--one").style.rotate = `${deg++}deg`;
-  if (deg == 360) deg = 0;
+  shapeOne.style.rotate = `${aboutDeg++}deg`;
+  if (aboutDeg == 360) aboutDeg = 0;
 }, 10);
 // End About
+
+// Client
+const shapeThree = document.querySelector(".client .shape--three");
+const shapeFour = document.querySelector(".client .shape--four");
+let clientCnt = 1;
+setInterval(() => {
+  if (clientCnt) {
+    shapeThree.classList.add("move");
+    shapeFour.classList.remove("move");
+  }
+  else {
+    shapeThree.classList.remove("move");
+    shapeFour.classList.add("move");
+  }
+  clientCnt = 1 - clientCnt;
+}, 2000);
+// End Client
+
+// Footer Top
+const shapeFive = document.querySelector(".footer-top .shape--five");
+const shapeSix = document.querySelector(".footer-top .shape--six");
+let footerDeg = 0, footerCheck = 0;
+setInterval(() => {
+  shapeFive.style.rotate = `${footerDeg}deg`;
+  shapeSix.style.rotate = `${footerDeg-10}deg`;
+  if (footerCheck == 0) {
+    ++footerDeg;
+    if (footerDeg == 30) {
+      footerCheck = 1;
+    }
+  }
+  else {
+    --footerDeg;
+    if (footerDeg == 0) {
+      footerCheck = 0;
+    }
+  }
+}, 100);
+// End Footer Top
